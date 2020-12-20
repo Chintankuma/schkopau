0666ab3e4762d68421e79b3cf922f629a3f437d6
chintan vaghani <chintanvaghani555@gmail.com>
2020-12-20 21:11:56 +0100

    basic routing completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import BuergerService from './BuergerService';
 
 function App() {
   return (
     <div className="app">
-      <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
+      <Router>
+        <Header></Header>
+        <Switch>
+        <Route path="/BuergerService">
+            <BuergerService></BuergerService>
+          </Route>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
